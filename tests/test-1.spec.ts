bc2516d042869fbd338111e4148f0bcbff204523
Fernando Oliveira <fernando_ho93@hotmail.com>
2026-03-11 00:10:53 -0300

    test: remove test-1 spec

--- tests/test-1.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('testes google ', async ({ page }) => {
-  await page.goto('https://www.google.com/?zx=1772290023119&no_sw_cr=1');
-  await page.getByRole('combobox', { name: 'Pesquisar' }).click();
-  await page.getByRole('combobox', { name: 'Pesquisar' }).fill('playwright');
-
-});
